41fc9245ca42797bb1b57cbf1e9c8ff0ad7e5192
Danial Owen <gydwo@leeds.ac.uk>
2018-09-21 10:05:54 +0100

    computing distance equation

--- distance.py
+++ distance.py
@@ -5,3 +5,12 @@ Created on Fri Sep 21 09:42:40 2018
 @author: gydwo
 """
 
+"""File for computing minimum distance between points"""
+
+
+import math
+
+
+def compute_distance(point1, point2):
+    return math.sqrt((point1[0] - point2[0])**2
+                     + (point1[1] - point2[1])**2)
